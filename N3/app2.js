5fa0896719d827fcdbb547a58059239243e33087
LAKE <lake.mentoria@gmail.com>
2021-03-06 05:25:48 -0300

    resumo de atv

--- N3/app2.js
+++ N3/app2.js
@@ -1,5 +1,15 @@
 var http = require('http');
-
-http.createServer(function(req,res){res.end("Hello Word! Welcome to My Website") }).listen(8081);
+//* http.createServer().listen(8081);
+http.createServer(function(req,res){res.end("Hello Word! Welcome to My Website!") }).listen(8081);
 
 console.log("O servidor esta capotando!!! Ê Tremm...")
+
+/*var express = require("express");
+var app = express();*/
+
+const express = require("express");
+const app = express();
+
+app.listen(8082, function(){
+                                   console.log("Servidor Ativo na Url http://localhost:8082");
+                                          });
