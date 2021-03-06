a70b9cf6b0ed005c615160de12d4d97010f09692
LAKE <lake.mentoria@gmail.com>
2021-03-06 04:38:36 -0300

    Create app2.js

---
+++ N3/app2.js
@@ -0,0 +1,5 @@
+var http = require('http');
+
+http.createServer(function(req,res){res.end("Hello Word! Welcome to My Website") }).listen(8081);
+
+console.log("O servidor esta capotando!!! Ê Tremm...")
