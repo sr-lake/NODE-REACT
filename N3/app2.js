7b690524b0ba44cae5bc59150b5e61a87780a839
LAKE <lake.mentoria@gmail.com>
2021-03-06 22:04:26 -0300

    mod app2

--- N3/app2.js
+++ N3/app2.js
@@ -7,8 +7,16 @@ console.log("O servidor esta capotando!!! Ê Tremm...")
 /*var express = require("express");
 var app = express();*/
 
+
+
 const express = require("express");
 const app = express();
+
+app.get("/", function(req, res){res.send ("Seja Bem-Vindo ao Meu App!")});
+
+app.get("/sobre", function(req, res){res.send ("Minha Pagina Sobre...")});
+
+app.get("/blog", function(req, res){res.send ("Seja Bem-Vindo ao Meu Bolg!.")});
 
 app.listen(8082, function(){
                                    console.log("Servidor Ativo na Url http://localhost:8082");
