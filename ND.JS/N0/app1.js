2d2a9cebf5452f37af538de3e9e4b80d3f807799
LAKE <lake.mentoria@gmail.com>
2021-03-09 17:44:48 -0300

    .0.01

--- ND.JS/N0/app1.js
+++ ND.JS/N0/app1.js
@@ -1,1 +1,15 @@
-console.log("Hello Word")
+const os = require ('os')
+const {freemem, totalmem} = os
+//console.log(`${parseInt(freemem() / 1024 / 1024)} MB` , totalmem())
+const total = parseInt(totalmem() / 1024 / 1024)
+const mem = parseInt(freemem() / 1024 / 1024)
+const percents = parseInt((mem / total) *100)
+
+const stats = 
+{
+    free: `${mem} MB`,
+    total `${total} MB`,
+    usage: `${percents}%`,
+}
+
+console.log(stats)
