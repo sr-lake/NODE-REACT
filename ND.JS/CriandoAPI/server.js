5a22da95561429c39db839af422d58f15fafecea
LAKE <lake.mentoria@gmail.com>
2021-03-11 22:39:40 -0300

    API teste01

--- ND.JS/CriandoAPI/server.js
+++ ND.JS/CriandoAPI/server.js
@@ -1,2 +1,21 @@
 
-console.log('Testando Lago...');
+const express = require ( 'express' ) ;
+const  debug  =  require ( 'debug' ) ( 'server : server' ) ;
+const  http  =  require ( 'http' ) ;
+
+const app = express();
+const port = 3000;
+app.set('port', port );
+
+const server = http.createServer(app);
+const router = express.router();
+
+  const route = router.get('/', (req, res, next)=> {
+        res.status(200).send({
+            title: "Node Store API", 
+            version: "0.0.1"
+        });
+  });
+
+  app.use('/', route);
+console.log('API rodando na porta...' + port);
