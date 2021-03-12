63a3f7bd71065eedc66df4d81a50deab8d377da4
LAKE <lake.mentoria@gmail.com>
2021-03-12 04:15:57 -0300

    Erro no CRUD 0.1

--- ND.JS/CApi/src/app.js
+++ ND.JS/CApi/src/app.js
@@ -1,15 +1,21 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const router = express.Router()
+const app = express();
+const router = express.Router();
 
-  const route = router.get('/', (req, res, next) => {
+  const route = router.get
+('/', (req, res, next) => 
+{
         res.status(200).send
         ({
             title: "SERVER LAKE - API", 
             version: "0.0.1"
         });
-  });
-  app.use('/', route)
-
-  module.exports = app
+});
+/* const create = router.post('/', (req, res, next) =>
+{
+      res.status(201).send(req.body);
+});*/
+app.use('/', route);
+//app.use('/product');
+module.exports = app;
