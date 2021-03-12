2dde1589ca393d2d854a0229ce9fe3663e5402a3
LAKE <lake.mentoria@gmail.com>
2021-03-12 04:58:39 -0300

    BCKP api.1

--- ND.JS/N2/app1.js
+++ ND.JS/N2/app1.js
@@ -17,3 +17,32 @@ function somar(a, b)
     return a + b
 }
 console.log (somar(n1, n2))
+
+
+
+
+
+
+//
+
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+  const route = router.get
+('/', (req, res, next) => 
+{
+        res.status(200).send
+        ({
+            title: "SERVER LAKE - API", 
+            version: "0.0.1"
+        });
+});
+/* const create = router.post('/', (req, res, next) =>
+{
+      res.status(201).send(req.body);
+});*/
+app.use('/', route);
+//app.use('/product');
+module.exports = app;
