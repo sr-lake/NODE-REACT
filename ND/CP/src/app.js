9cba3032a9a0b638e904af99c4955eb04bbaa7ba
LAKE <lake.mentoria@gmail.com>
2021-03-13 02:06:06 -0300

    NDmon & CRUD com sucesso!
    Aplicação Crud criada como MD. para uso. Teste de nodemon ok.

--- ND/CP/src/app.js
+++ ND/CP/src/app.js
@@ -28,14 +28,28 @@ app.use(bodyParser.urlencoded({extended: false}))
     }
 );
 
-  const put = router.put
+ const put = router.put
 ('/:id', (req, res, next) => 
     {
         const id = req.params.id;
-        res.status(201).send(req.body);
+        res.status(201).send
+        ({
+            id: id,
+            item: req.body
+        });
     }
 );
+
+ const del = router.delete
+ ('/', (req, res, next) => 
+    {
+     res.status(200).send(req.body);
+    }
+);
+
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
