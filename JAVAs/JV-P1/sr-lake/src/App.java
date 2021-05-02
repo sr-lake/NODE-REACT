b99d03cebac74c8ff7cf7ae60934cc50a9227356
LAKE <lake.mentoria@gmail.com>
2021-05-02 17:02:20 -0300

    .

--- JAVAs/JV-P1/sr-lake/src/App.java
+++ JAVAs/JV-P1/sr-lake/src/App.java
@@ -2,11 +2,12 @@ package App ;
 
 import pacote1.Dog;
 import pacote2.person;
+
 public class App {
-    public static void public static void main(String[] args) throws Exception { 
+    public static void main(String[] args) throws Exception { 
         System.out.println("Lake Mentoria rodando JAVA!");
         Dog d1 = new Dog();
-        person p1 = new person();
+        person p1 = new Person();
         d1.bark();
         p1.hello();
     }
--- JAVAs/JV-P1/sr-lake/src/pacote2/person.java
+++ JAVAs/JV-P1/sr-lake/src/pacote2/person.java
@@ -2,6 +2,6 @@ package pacote2;
 
     public class person {
             public void hello() {
-                System.out.println("Teste do Java no Servidor LAKE.");
-        }
+                System.out.println("Teste do Java no Servidor LAKE.")
+}
 }
